264b3b3c45410d74e988befc0bff45f6c8312fd4
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-18 11:55:32 +0545

    view added for products page

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def mainHome(request):
     return render(request, 'main/home.html')
+
+def products(request):
+    return render(request, 'main/products.html')
