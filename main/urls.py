505dc6892ee297c9cf71dbe2c1ba7c54a314d5a3
Kshamata Pariyar <Kshamata12@gmail.com>
2024-09-20 17:03:51 +0545

    urls of product-detail and catalogue added

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 
     path('', views.mainHome, name='main-home'),
     path('products/', views.products, name='main-products'),
+    path('product-detail/', views.productDetail, name='main-product-detail'),
+    path('catalogue/', views.catalogue, name='main-catalogue'),
 
 ]
